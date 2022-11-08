b5cb53b5bb2224ee0b6d3b7b6a23db46af41dc3c
ImtiazUddin28 <imtiaz100463@gmail.com>
2022-11-08 09:06:58 +0600

    all essential package is installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
       <h1>Insallah, project will be done within two days</h1>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
     </div>
   );
 }
